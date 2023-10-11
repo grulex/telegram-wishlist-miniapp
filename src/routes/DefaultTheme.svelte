823f6d6fcca9b0f159132b9a7ffece674710d041
Vladislav <vlados_pvo@mail.ru>
2023-10-11 16:46:46 +0500

    1 create

--- src/routes/DefaultTheme.svelte
+++ src/routes/DefaultTheme.svelte
@@ -1,11 +1,14 @@
 <style>
   :global(:root) {
-    --tg-theme-bg-color: #232e3c;
-    --tg-theme-secondary-bg-color: #17212b;
+    --tg-color-scheme: dark;
+    --tg-theme-bg-color: #17212b;
     --tg-theme-button-color: #5288c1;
     --tg-theme-button-text-color: #ffffff;
     --tg-theme-hint-color: #708499;
     --tg-theme-link-color: #6ab3f3;
+    --tg-theme-secondary-bg-color: #232e3c;
     --tg-theme-text-color: #f5f5f5;
+    --tg-viewport-height: 496px;
+    --tg-viewport-stable-height: 496px;
   }
 </style>
