d9d55a4ec6f42b695c2a163014ee9f108036d813
grulex <god@vk.com>
2024-05-18 23:37:51 +0100

    small fixes

--- src/routes/+layout.server.js
+++ src/routes/+layout.server.js
@@ -1,13 +1,13 @@
 /** /src/lib/other.js **/
 import { env } from '$lib/env'
 
-export async function load({ cookies, headers }) {
+export async function load({ cookies, request }) {
     let token = cookies.get("tg_init_data");
     if (env.TG_DEV_INIT_DATA_BASE64) {
         token = env.TG_DEV_INIT_DATA_BASE64;
     }
     // get header Sf-Ipcountry
-    let country = headers['sf-ipcountry'];
+    let country = request.headers.get('CF-IPCountry');
     console.log('country', country);
 
     let resp = await fetch(env.BACKEND_HOST + '/api/profile', {
