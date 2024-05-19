8f82cec554dfce6e07d3a1118bff6746c6d2d8b3
grulex <god@vk.com>
2024-05-19 17:04:38 +0100

    Fixes for dev environment

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,29 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import localtunnel from 'vite-plugin-localtunnel'
 
 export default defineConfig(({mode}) => {
+	const env = loadEnv(mode, process.cwd());
+
 	return {
 		plugins: [
 			sveltekit(),
 			startLocalTunnel(mode),
 		],
+		server: {
+			port: `${env.PORT ?? '5173'}`,
+		},
 	}
 })
 
 function startLocalTunnel(mode) {
+	const env = loadEnv(mode, process.cwd());
+
 	if (mode !== 'development') {
 		return
 	}
 	return localtunnel({
-		subdomain: 'wishlist',
-		port: 5173,
+		subdomain: `${env.TUNNEL_SUBDOMAIN ?? 'wishlist'}`,
+		port: `${env.PORT ?? '5173'}`,
 	})
 }
